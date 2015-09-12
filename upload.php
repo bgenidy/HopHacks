843f407190901a52559d988d75496518675f0d35
victoriaz8 <victoriazheng@gwu.edu>
2015-09-12 14:33:00 -0400

    added vardump catch

--- upload.php
+++ upload.php
@@ -4,6 +4,10 @@ $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 // Check if image file is a actual image or fake image
+
+echo "Hi";
+var_dump($_POST);
+
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
